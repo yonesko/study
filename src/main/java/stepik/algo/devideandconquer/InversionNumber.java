3336677daa1f58326a3e2a5573bfd2a6bda70c0b
gdanichev <gitgle66>
2017-08-28 13:09:34 +0300

    Inversion number

--- src/main/java/stepik/algo/devideandconquer/InversionNumber.java
+++ src/main/java/stepik/algo/devideandconquer/InversionNumber.java
@@ -1,15 +1,65 @@
 package stepik.algo.devideandconquer;
 
-public class InversionNumber {
-    static int count(int arr[]) {
-        int ret = 0;
+import java.util.*;
+import java.util.stream.Collectors;
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) ret++;
+public class InversionNumber {
+    private static int invNum = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[sc.nextInt()];
+
+        for (int i = 0; i < arr.length; i++) arr[i] = sc.nextInt();
+
+        System.out.println(count(arr));
+    }
+
+    static List<Integer> count(List<Integer> arr) {
+        int m = arr.size() / 2;
+        return merge(mergeSort(arr.subList(0, m)), mergeSort(arr.subList(m, arr.size())));
+    }
+
+    static List<Integer> merge(List<Integer> a, List<Integer> b) {
+        List<Integer> ret = new ArrayList<>(a.size() + b.size());
+
+        int ia = 0, ib = 0;
+        for (; ia < a.size() && ib < b.size(); ) {
+            if (a.get(ia) > b.get(ib)) {
+                invNum += a.size() - ia;
+                ret.add(b.get(ib++));
+            } else {
+                ret.add(a.get(ia++));
             }
         }
 
+        for (int i = ia; i < a.size(); i++) ret.add(a.get(i));
+        for (int i = ib; i < b.size(); i++) ret.add(b.get(i));
+
         return ret;
+    }
+
+    static List<Integer> mergeSort(List<Integer> arr) {
+        if (arr.size() < 2) return arr;
+
+        if (arr.size() == 2) {
+            if (arr.get(0) > arr.get(1)) {
+                Collections.swap(arr, 0, 1);
+                invNum++;
+                return arr;
+            } else return arr;
+        }
+        int m = arr.size() / 2;
+        return merge(mergeSort(arr.subList(0, m)), mergeSort(arr.subList(m, arr.size())));
+    }
+
+    static int count(int arr[]) {
+        invNum = 0;
+
+        count(Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.toList()));
+
+        return invNum;
     }
 }
--- src/test/java/stepik/algo/devideandconquer/InversionNumberTest.java
+++ src/test/java/stepik/algo/devideandconquer/InversionNumberTest.java
@@ -1,12 +1,28 @@
 package stepik.algo.devideandconquer;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Comparator;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
+import static stepik.algo.devideandconquer.InversionNumber.merge;
+import static stepik.algo.devideandconquer.InversionNumber.mergeSort;
+
 public class InversionNumberTest {
+    @Test
+    public void invNumber() throws Exception {
+        int arr[] = {2, 3, 9, 2, 9};
+        assertEquals(2, InversionNumber.count(arr));
+
+        arr = new int[]{12,56,23,8,4};
+        assertEquals(8, InversionNumber.count(arr));
+    }
+
     @Test
     public void reversed() throws Exception {
         int nums = 100;
@@ -16,7 +32,28 @@ public class InversionNumberTest {
                 .mapToInt(Integer::intValue)
                 .toArray();
 
-        Assert.assertEquals(nums * (nums - 1) / 2, InversionNumber.count(arr));
+        assertEquals(nums * (nums - 1) / 2, InversionNumber.count(arr));
+    }
+
+    @Test
+    public void reversed2() throws Exception {
+        int nums = 10;
+        int arr[] = IntStream.range(0, nums)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        assertEquals(nums * (nums - 1) / 2, InversionNumber.count(arr));
+    }
+
+    @Test(timeout = 2000)
+    public void many() {
+        int arr[] = IntStream.range(0, (int) 1e6)
+                .boxed()
+                .mapToInt(Integer::intValue)
+                .toArray();
+        assertEquals(0, InversionNumber.count(arr));
     }
 
     @Test
@@ -26,24 +63,52 @@ public class InversionNumberTest {
                 .mapToInt(Integer::intValue)
                 .toArray();
 
-        Assert.assertEquals(0, InversionNumber.count(arr));
+        assertEquals(0, InversionNumber.count(arr));
     }
 
     @Test
     public void empty() {
         int arr[] = {};
-        Assert.assertEquals(0, InversionNumber.count(arr));
+        assertEquals(0, InversionNumber.count(arr));
     }
 
     @Test
     public void one() {
         int arr[] = {1};
-        Assert.assertEquals(0, InversionNumber.count(arr));
+        assertEquals(0, InversionNumber.count(arr));
     }
 
     @Test
     public void two() {
         int arr[] = {2, 1};
-        Assert.assertEquals(1, InversionNumber.count(arr));
+        assertEquals(1, InversionNumber.count(arr));
+    }
+
+    @Test
+    public void mergeTest() {
+        assertEquals(asList(1, 1), merge(singletonList(1), singletonList(1)));
+        assertEquals(singletonList(1), merge(emptyList(), singletonList(1)));
+        assertEquals(asList(-34, 1, 2, 99), merge(asList(1, 2), asList(-34, 99)));
+        assertEquals(asList(-34, 1, 2, 3, 7, 99),
+                merge(
+                        merge(asList(1, 2), asList(-34, 99)),
+                        asList(3, 7)));
+        assertEquals(asList(-34, 1, 2, 3, 7, 99, 101, 102, 103, 104),
+                merge(
+                        merge(asList(1, 2, 101, 102, 103, 104), asList(-34, 99)),
+                        asList(3, 7)));
+    }
+
+    @Test
+    public void mergeSortTest() {
+        assertEquals(asList(1, 2), mergeSort(asList(2, 1)));
+        assertEquals(asList(1, 2), mergeSort(asList(1, 2)));
+        assertEquals(emptyList(), mergeSort(emptyList()));
+
+        List<Integer> randInts = new Random().ints(1000).boxed().collect(Collectors.toList());
+        List<Integer> sortedInts = new ArrayList<>(randInts);
+        Collections.sort(sortedInts);
+
+        assertEquals(sortedInts, mergeSort(randInts));
     }
 }
